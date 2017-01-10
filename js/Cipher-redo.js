f8af8de0dbd2f56fe9d398d50b78182603b3f8e0
Kory Skarbek and Melvin Gruschow <melvin@>
2017-01-09 16:36:53 -0800

    redid cipher.js with regex

--- js/Cipher-redo.js
+++ js/Cipher-redo.js
@@ -1,13 +1,16 @@
 var userSent = prompt("Type a sentence");
 var first = userSent.search(/^[a-z]/i);
-  // var firstLetter = /^[a-z]/i.test(userSent);
 var firstLetter = userSent.charAt(first).toUpperCase();
-console.log(firstLetter);
+//console.log(firstLetter);
 
 var last = userSent.search(/[a-z]$/i);
 var lastLetter = userSent.charAt(last).toUpperCase();
-console.log (lastLetter);
+//console.log (lastLetter);
 
 var edited = userSent + firstLetter + lastLetter;
 
-console.log(edited);
+//console.log(edited);
+
+var reverse = function(){
+  return userSent + lastLetter + firstLetter;
+}
